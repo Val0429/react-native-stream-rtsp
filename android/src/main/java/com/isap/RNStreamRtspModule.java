43248ef41c7064e692c0fd77112ae61da048e23d
neo <neo>
2018-11-14 22:35:22 +0800

    Initial Commit

---
+++ android/src/main/java/com/isap/RNStreamRtspModule.java
@@ -0,0 +1,22 @@
+
+package com.isap;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+
+public class RNStreamRtspModule extends ReactContextBaseJavaModule {
+
+  private final ReactApplicationContext reactContext;
+
+  public RNStreamRtspModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.reactContext = reactContext;
+  }
+
+  @Override
+  public String getName() {
+    return "RNStreamRtsp";
+  }
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+
+import { NativeModules } from 'react-native';
+
+const { RNStreamRtsp } = NativeModules;
+
+export default RNStreamRtsp;
